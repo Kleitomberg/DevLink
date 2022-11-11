a8ffcceea23296b32729161e378c6d53d712d8c7
kleitomberg <kleitom-1@hotmail.com>
2022-11-11 10:19:07 -0300

    exibindo sociais links apenas se existir

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,11 +4,11 @@ import { Social } from '../../components/social-icons'
 import { FaFacebook, FaInstagram, FaYoutube, FaGithub,FaLinkedin  } from 'react-icons/fa'
 import { db } from '../../services/firebase'
 import { useEffect,useState } from 'react'
-import { collection,query,orderBy, getDocs, } from 'firebase/firestore'
+import { collection,query,orderBy, getDocs, getDoc, doc } from 'firebase/firestore'
 export default function Home(){
 
     const [links, setLinks] = useState([]);
-    const [social, setSocial] = useState([]);
+    const [social, setSocial] = useState({});
 
     useEffect(() => {
 
@@ -39,6 +39,27 @@ export default function Home(){
 
     }, [])
 
+    useEffect(() => {
+
+            function getSocial(){
+                const mySocialLinks = doc(db, 'social', 'links'); //pega os links do banco de dados
+                getDoc(mySocialLinks)
+                .then((snapshot) => {
+
+                    if(snapshot.exists()){
+                        setSocial({
+                            instagram: snapshot.data().instagram,
+                            youtube: snapshot.data().youtube,
+                            linkedin: snapshot.data().linkeding,
+                        })
+
+                    }
+
+                })
+
+            }
+            getSocial()
+    }, [])
 
     return (
         <div className='home-container'>
@@ -54,17 +75,26 @@ export default function Home(){
                     )
                 })}
 
-                <footer className='icones'>
-                    <Social url="https://www.linkedin.com/in/kleitomberg/">
+
+
+                    { links.length !== 0  && Object.keys(social).length > 0 && (
+                        <footer className='icones'>
+                        <Social url={social?.linkedin}>
                         <FaLinkedin size={35} color='#fff'/>
-                    </Social>
-                    <Social url="https://github.com/Kleitomberg">
-                        <FaGithub size={35} color='#fff'/>
-                    </Social>
-                    <Social url="https://www.instagram.com/kleitomberg/">
+                        </Social>
+
+                        <Social url={social?.youtube}>
+                        <FaYoutube size={35} color='#fff'/>
+                        </Social>
+
+                        <Social url={social?.instagram}>
                         <FaInstagram size={35} color='#fff'/>
-                    </Social>
-                </footer>
+                        </Social>
+                        </footer>
+                    )}
+
+
+
             </main>
         </div>
     )
